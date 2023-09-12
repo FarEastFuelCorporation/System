ffbe096216ca898a9e676bac7eb292967d15b6a6
jimfalar@gmail.com <jimfalar@gmail.com>
2023-09-12 08:41:13 +0800

    Update Files

--- public/js/UserAccounts.js
+++ public/js/UserAccounts.js
@@ -1,32 +1,45 @@
-fetch('https://script.google.com/macros/s/AKfycbwmA97K4sdfq6dhzSsp14JU9KgQrFgSARNZbvSfiU7vuH8oEipt6TmcFo_o-jCI0kiQ/exec')
-  .then(res => res.json())
-  .then(data => {
-    const submitButton = document.getElementById("submit_button");
-    const form = document.forms.login;
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-    });
+document.addEventListener('DOMContentLoaded', async function() {
+  try {
+      const username_response_promise = fetch('https://script.google.com/macros/s/AKfycbwmA97K4sdfq6dhzSsp14JU9KgQrFgSARNZbvSfiU7vuH8oEipt6TmcFo_o-jCI0kiQ/exec');
 
-    submitButton.addEventListener("click", () => {
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
+      const [
+          username_response,
+      ] = await Promise.all([
+          username_response_promise,
+      ]);
 
-      let user = null;
-      for (let x = 1; x < data.content.length; x++) {
-        if (username === data.content[x][0] && password === data.content[x][1]) {
-          user = data.content[x][2];
-          break;
+      const username_data_list  = await username_response.json();
+
+      const submitButton = document.getElementById("submit_button");
+      const form = document.forms.login;
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+      });
+  
+      submitButton.addEventListener("click", () => {
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+  
+        let user = null;
+        for (let x = 1; x < username_data_list.content.length; x++) {
+          if (username === username_data_list.content[x][findTextInArray(username_data_list, "USERNAME")] && password === username_data_list.content[x][findTextInArray(username_data_list, "PASSWORD")]) {
+            user = data.content[x][findTextInArray(username_data_list, "ACCOUNT")];
+            break;
+          }
         }
-      }
+  
+        if (user) {
+          window.location.assign(`public/users/${user}.html`);
+          console.log("authenticated");
+          document.getElementById('error-message').innerText = '';
+        } else {
+          document.getElementById('error-message').innerText = 'Username and Password did not match';
+          document.getElementById('error-message').style.color = 'red';
+          console.log('Username and Password did not match');
+        }
+      });
 
-      if (user) {
-        window.location.assign(`public/users/${user}.html`);
-        console.log("authenticated");
-        document.getElementById('error-message').innerText = '';
-      } else {
-        document.getElementById('error-message').innerText = 'Username and Password did not match';
-        document.getElementById('error-message').style.color = 'red';
-        console.log('Username and Password did not match');
-      }
-    });
-  });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+  }
+});
