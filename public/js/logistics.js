6e86e3a686afeed35e52e24630a92c28a9eef50f
jimfalar@gmail.com <jimfalar@gmail.com>
2023-11-29 21:31:55 +0800

    Update logistics.js

--- public/js/logistics.js
+++ public/js/logistics.js
@@ -84,18 +84,20 @@ document.addEventListener('DOMContentLoaded', async function() {
 
         // Code that depends on the fetched data
         // username_data_list
+        const queryParams = new URLSearchParams(window.location.search);
+        const xValue = queryParams.get('user');
         const user_sidebar = document.getElementById("user_sidebar");
         const user_sidebar_officer = document.getElementById("user_sidebar_officer");
         const user_sidebar_department = document.getElementById("user_sidebar_department");
         const users = document.querySelectorAll("#user");
         const departments = document.querySelectorAll("#department");
 
-        const user_name = username_data_list.content[7][findTextInArray(username_data_list, "NAME")];
-        users.forEach(user => {user.value = username_data_list.content[7][findTextInArray(username_data_list, "NAME")]})
-        departments.forEach(user => {user.value = username_data_list.content[1][findTextInArray(username_data_list, "DEPARTMENT")]})
-        user_sidebar.innerHTML = `<u>${username_data_list.content[7][findTextInArray(username_data_list, "NAME")]}</u>`;
-        user_sidebar_officer.innerText = username_data_list.content[7][findTextInArray(username_data_list, "SECTIONS")];
-        user_sidebar_department.innerText = username_data_list.content[7][findTextInArray(username_data_list, "DEPARTMENT")];
+        const user_name = username_data_list.content[xValue][findTextInArray(username_data_list, "NAME")];
+        users.forEach(user => {user.value = username_data_list.content[xValue][findTextInArray(username_data_list, "NAME")]})
+        departments.forEach(user => {user.value = username_data_list.content[xValue][findTextInArray(username_data_list, "DEPARTMENT")]})
+        user_sidebar.innerHTML = `<u>${username_data_list.content[xValue][findTextInArray(username_data_list, "NAME")]}</u>`;
+        user_sidebar_officer.innerText = username_data_list.content[xValue][findTextInArray(username_data_list, "SECTIONS")];
+        user_sidebar_department.innerText = username_data_list.content[xValue][findTextInArray(username_data_list, "DEPARTMENT")];
         
         // logistic_dashboard
         const logistic_dashboard = document.querySelector("#logistic_dashboard");
