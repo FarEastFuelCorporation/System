517ea69ee2d865fd738da696e3e13c776d394414
jimfalar@gmail.com <jimfalar@gmail.com>
2023-12-07 21:58:54 +0800

    Update Files

--- public/js/employee.js
+++ public/js/employee.js
@@ -3,20 +3,28 @@ document.addEventListener('DOMContentLoaded', async function() {
         const username_response_promise = fetch('https://script.google.com/macros/s/AKfycbwmA97K4sdfq6dhzSsp14JU9KgQrFgSARNZbvSfiU7vuH8oEipt6TmcFo_o-jCI0kiQ/exec');
         const leave_response_promise = fetch('https://script.google.com/macros/s/AKfycbyBmD5-PmLKn61bDo7WweODJM703OsC7hIroNwwW7d2lKCWNM1Ka8aF4ZRzZ-gvyniAxw/exec');
         const employee_response_promise = fetch('https://script.google.com/macros/s/AKfycbwns5R6TA8U64ywbb9hwYu4LKurAjTM0Z18NYNZMt0Ft0m-_NUHYbYqblk_5KWugvt7lA/exec');
+        const overtime_undertime_response_promise = fetch('https://script.google.com/macros/s/AKfycbzQMLI5voLzH7EiaRWyWtfS-kSovXWTBLDJaLmCL8-zJ25EOEsPN1ISlvlHcXlIylIs/exec');
+        const loans_response_promise = fetch('https://script.google.com/macros/s/AKfycbwnHzcfjHaD6xRdvea58nU0MuvmIdrpLr73TKXiAo3kV6l7YVFUQoLV7FzDhzgSw4pB/exec');
 
         const [
             username_response,
             leave_response,
             employee_response,
+            overtime_undertime_response,
+            loans_response,
         ] = await Promise.all([
             username_response_promise,
             leave_response_promise,
             employee_response_promise,
+            overtime_undertime_response_promise,
+            loans_response_promise,
         ]);
 
         const username_data_list  = await username_response.json();
         const leave_data_list  = await leave_response.json();
         const employee_data_list  = await employee_response.json();
+        const ot_ut_data_list  = await overtime_undertime_response.json();
+        const loan_data_list  = await loans_response.json();
 
         // Code that depends on the fetched data
         // username_data_list
@@ -45,7 +53,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         user_sidebar_officer.innerText = username_data_list.content[xValue][findTextInArray(username_data_list, "SECTIONS")];
         user_sidebar_department.innerText = username_data_list.content[xValue][findTextInArray(username_data_list, "DEPARTMENT")];
         user_sidebar_employee_type.innerText = username_data_list.content[xValue][findTextInArray(username_data_list, "EMPLOYEE TYPE")];
-        console.log(username_data_list.content[xValue][findTextInArray(username_data_list, "EMPLOYEE TYPE")])
+
         // application_section
         const application_section = document.querySelector("#application_section");
         const buttons = application_section.querySelectorAll("button");
@@ -53,14 +61,17 @@ document.addEventListener('DOMContentLoaded', async function() {
         const vacation_button = buttons[1];
         const undertime_button = buttons[2];
         const overtime_button = buttons[3];
+        const cash_advance_button = buttons[4];
         const sick_leave_tab = application_section.querySelector("#sick_leave_tab");
         const vacation_leave_tab = application_section.querySelector("#vacation_leave_tab");
         const undertime_tab = application_section.querySelector("#undertime_tab");
         const overtime_tab = application_section.querySelector("#overtime_tab");
+        const cash_advance_tab = application_section.querySelector("#cash_advance_tab");
         const back1 = sick_leave_tab.querySelector("#back");
         const back2 = vacation_leave_tab.querySelector("#back");
         const back3 = undertime_tab.querySelector("#back");
         const back4 = overtime_tab.querySelector("#back");
+        const back5 = cash_advance_tab.querySelector("#back");
 
         sick_leave_button.addEventListener("click", () => {
             sick_leave_tab.style.display = "block"
@@ -74,6 +85,9 @@ document.addEventListener('DOMContentLoaded', async function() {
         overtime_button.addEventListener("click", () => {
             overtime_tab.style.display = "block"
         })
+        cash_advance_button.addEventListener("click", () => {
+            cash_advance_tab.style.display = "block"
+        })
 
         back1.addEventListener("click", () => {
             sick_leave_tab.style.display = "none"
@@ -86,6 +100,9 @@ document.addEventListener('DOMContentLoaded', async function() {
         })
         back4.addEventListener("click", () => {
             overtime_tab.style.display = "none"
+        })
+        back5.addEventListener("click", () => {
+            cash_advance_tab.style.display = "none"
         })
 
         for(let x = 1; x < employee_data_list.content.length; x++){
