38976860b64861f6d456eb0793f98d93a43d7277
jimfalar@gmail.com <jimfalar@gmail.com>
2024-01-25 11:45:56 +0800

    Update user5.js

--- public/js/user5.js
+++ public/js/user5.js
@@ -759,9 +759,7 @@ document.addEventListener('DOMContentLoaded', async function() {
                             si_tin_nature_of_business_container.innerText = `${nature_of_business}`;
                             si_date_container.innerText = date_of_certification;
                             if(cod_data_list.content[x][findTextInArray(cod_data_list, "WASTE NAME")] == "CARDBOARDS"){
-                                if(!vehicles.includes(plate_no)){
-                                    is_transportation = false
-                                }
+                                is_transportation = false
                             }
                             if(max_capacity == 0){
                                 if(mode != "FREE OF CHARGE"){
